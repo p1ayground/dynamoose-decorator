07d23a6ad703ae0dca9a0e87b26f7a2a05f626ce
changmyeong <changmyeong.baek@gmail.com>
2024-01-24 23:27:39 +0900

    refactor: fix typo

--- src/decorators/schema.ts
+++ src/decorators/schema.ts
@@ -2,17 +2,17 @@ import * as dynamoose from 'dynamoose'
 import { Item } from 'dynamoose/dist/Item'
 
 import { SchemaSettings } from '../type'
-import { getAttributeDefinitions, getSchmeaSettings, setSchmeaSettings } from '../utils/reflect-metadata'
+import { getAttributeDefinitions, getSchemaSettings, setSchemaSettings } from '../utils/reflect-metadata'
 
 export const Schema =
   (settings: SchemaSettings = {}) =>
   (target: any) => {
-    setSchmeaSettings(target.constructor, settings)
+    setSchemaSettings(target.constructor, settings)
   }
 
 export function getSchema<T extends Item>(model: { new (...args: any): T }) {
   const definitions = getAttributeDefinitions(model)
-  const settings = getSchmeaSettings(model)
+  const settings = getSchemaSettings(model)
 
   const keys = Object.keys(definitions)
   for (const key of keys) {
